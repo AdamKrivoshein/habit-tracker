bf57d5f4c7a7923dd9daafb50afb2b95814584cc
Adam Krivoshein <adamskrivo@gmail.com>
2024-12-17 13:56:26 -0800

    Implemented bare minimum reducer for a single day.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import { useReducer } from 'react'
+
+interface State {
+  success: boolean
+}
+
+type DayAction =
+  | { type: "reset" }
+  | { type: "setSuccess"; value: State["success"] }
+
+const initialState: State = { success: false };
+
+function stateReducer(state: State, action: DayAction): State {
+  switch (action.type) {
+    case "reset":
+      return initialState;
+    case "setSuccess":
+      return { ...state, success: action.value };
+    default:
+      throw new Error("Unknown action");
+  }
+}
+
+
 interface Day {
   date: Date;
   status: boolean;
@@ -20,20 +46,31 @@ function Habit({ aHabit }: { aHabit: Day }) {
 }
 
 export default function Home() {
+  const [state, dispatch] = useReducer(stateReducer, initialState);
+  const markSuccess = () => dispatch({ type: "setSuccess", value: true })
+  const reset = () => dispatch({ type: "reset" })
+
+
   const habits = [];
-  let habit: Day[] = [];
+  const habit: Day[] = [];
   habit.push({ date: new Date(2024, 1, 1), status: false, id: 1 })
   habit.push({ date: new Date(2024, 1, 2), status: false, id: 2 })
   habit.push({ date: new Date(2024, 1, 3), status: false, id: 3 })
 
-  // const day: Day = {
-  //   date: new Date(2024, 1, 1),
-  //   status: false
-  // }
+  function handleClick() {
+    habit.push({ date: new Date(2024, 1, 4), status: false, id: 4 })
+  }
   
+
   return (
     <div>
       <main>
+        <p>Success: {state.success.toString()}</p>
+        <button onClick={markSuccess}>Completed!</button>
+        <br />
+        <button onClick={reset}>Reset</button>
+        <br />
+        <br />
         <Habit aHabit={habit} />
       </main>
       <br />
