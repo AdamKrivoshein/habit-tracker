584c75e5f3efac6240e03b6d93ae56473f119148
Adam Krivoshein <adamskrivo@gmail.com>
2024-12-16 22:28:45 -0800

    Now passing habit array of Days instead of Days.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,43 @@
 interface Day {
   date: Date;
-  status: boolean
+  status: boolean;
 }
 
-function Day({ text }: { text: Day}) {
+function Day({ aDay }: { aDay: Day}) {
   return (
     <div>
-      <p>{text.date.toDateString()}</p>
+      <p>{aDay.date.toDateString()} = {aDay.status.toString()}</p>
     </div>
   )
 }
 
-function Habit({ aDay }: { aDay: Day }) {
+function Habit({ aHabit }: { aHabit: Day }) {
   return (
     <ul>
-      <Day text={aDay} />
-      <Day text={aDay} />
+      <Day aDay={aHabit[0]} />
+      <Day aDay={aHabit[1]} />
+      <Day aDay={aHabit[2]} />
+      <button>+</button>
     </ul>
   )
 }
 
 export default function Home() {
-  const day: Day = {
-    date: new Date(2024, 1),
-    status: false
-  }
-  const habit = []
-  const habits = []
+  const habits = [];
+  let habit: Day[] = [];
+  habit.push({ date: new Date(2024, 1, 1), status: false })
+  habit.push({ date: new Date(2024, 1, 2), status: false })
+  habit.push({ date: new Date(2024, 1, 3), status: false })
+
+  // const day: Day = {
+  //   date: new Date(2024, 1, 1),
+  //   status: false
+  // }
+  
   return (
     <div>
       <main>
-        <Habit aDay={day} />
-        <Habit aDay={day} />
+        <Habit aHabit={habit} />
       </main>
       <br />
       <footer>
