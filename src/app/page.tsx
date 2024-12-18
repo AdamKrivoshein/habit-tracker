797a509674de76babd3ad5e37bde14bbc5aaa24f
Adam Krivoshein <adamskrivo@gmail.com>
2024-12-18 13:40:39 -0800

    Refactored the reducer with better functionality.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,14 @@ import { useReducer } from 'react'
 
 interface State {
   success: boolean
+  date: Date
 }
 
 type DayAction =
   | { type: "reset" }
   | { type: "setSuccess"; value: State["success"] }
 
-const initialState: State = { success: false };
+const initialState: State = { success: false, date: new Date(2024, 1, 1) };
 
 function stateReducer(state: State, action: DayAction): State {
   switch (action.type) {
@@ -23,59 +24,24 @@ function stateReducer(state: State, action: DayAction): State {
   }
 }
 
-
-interface Day {
-  date: Date;
-  status: boolean;
-  id: number
-}
-
-function Habit({ aHabit }: { aHabit: Day }) {
-  const daysList = aHabit.map(aHabit =>
-    <li key={aHabit.id}>
-      {aHabit.date.toDateString()} = {aHabit.status.toString()}
-    </li>
-  )
-
-  return (
-    <ul>
-      {daysList}
-      <li><button>+</button></li>
-    </ul>
-  )
-}
-
 export default function Home() {
   const [state, dispatch] = useReducer(stateReducer, initialState);
-  const markSuccess = () => dispatch({ type: "setSuccess", value: true })
-  const reset = () => dispatch({ type: "reset" })
-
-
-  const habits = [];
-  const habit: Day[] = [];
-  habit.push({ date: new Date(2024, 1, 1), status: false, id: 1 })
-  habit.push({ date: new Date(2024, 1, 2), status: false, id: 2 })
-  habit.push({ date: new Date(2024, 1, 3), status: false, id: 3 })
-
-  function handleClick() {
-    habit.push({ date: new Date(2024, 1, 4), status: false, id: 4 })
-  }
-  
+  const markSuccess = (successValue: boolean) => dispatch({ type: "setSuccess", value: successValue })
+  const reset = () => dispatch({ type: "reset" })  
 
   return (
     <div>
       <main>
-        <p>Success: {state.success.toString()}</p>
-        <button onClick={markSuccess}>Completed!</button>
+        <p>{state.date.toDateString()}: {state.success.toString()}</p>
+        <button onClick={() => markSuccess(true)}>Completed!</button>
         <br />
-        <button onClick={reset}>Reset</button>
+        <button onClick={() => markSuccess(false)}>Mission failed!</button>
         <br />
-        <br />
-        <Habit aHabit={habit} />
       </main>
       <br />
+      <br />
       <footer>
-        <p>Footer</p>
+        <p>A habit tracker</p>
       </footer>
     </div>
   );
