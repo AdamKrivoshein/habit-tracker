dbde411236754eaba3e1e1c081c1a5211d8cf6c3
Adam <adamskrivo@gmail.com>
2024-12-29 10:58:03 -0800

    Changed date UI to be buttons. Clicking dates now toggles the success value.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,25 +13,32 @@ interface State {
 
 type DayAction =
   | { type: "reset" }
-  | { type: "setSuccess"; value: State["days"][0]["success"] }
+  | { type: "addSuccess"; value: State["days"][0]["success"] }
+  | { type: "toggleSuccess"; value: State["days"][0]["date"] }
 
-const initialState: State = { days: [ { success: false, date: new Date(2024, 1, 1) } ] };
+const initialState: State = { days: [ { success: false, date: new Date(2024, 0, 1) } ] };
 
 function stateReducer(state: State, action: DayAction): State {
   switch (action.type) {
     case "reset":
       return initialState;
-    case "setSuccess":
+    case "addSuccess":
       return {
         ...state,
         days: [...state.days, { success: action.value, date: new Date() }]
-      };
-    /*case "addSuccess":
-      state.days[0] = 
+      }
+    case "toggleSuccess":
+      const updatedDays = state.days.map(day => {
+        if (day.date.getTime() === action.value.getTime()) {
+          return { ...day, success: !day.success };
+        }
+        return day;
+      })
+
       return {
         ...state,
-        days: [...state.days, state.days[0].success: {success: action.value} ]
-      }*/
+        days: updatedDays
+      }
     default:
       throw new Error("Unknown action");
   }
@@ -39,18 +46,20 @@ function stateReducer(state: State, action: DayAction): State {
 
 export default function Home() {
   const [state, dispatch] = useReducer(stateReducer, initialState);
-  const markSuccess = (successValue: boolean) => dispatch({ type: "setSuccess", value: successValue })
+  const addSuccess = (successValue: boolean) => dispatch({ type: "addSuccess", value: successValue })
+  const toggleSuccess = (dateValue: Date) => dispatch({ type: "toggleSuccess", value: dateValue })
   const reset = () => dispatch({ type: "reset" })  
 
   return (
     <div>
       <main>
         {state.days.map((day, index) => (
-          <p key={index}>{day.date.toDateString()}: {day.success.toString()}</p>
+          <button key={index} onClick={() => toggleSuccess(day.date)}>- - {day.date.toDateString()}: {day.success.toString()} - -</button>
         ))}
-        <button onClick={() => markSuccess(true)}>Completed!</button>
         <br />
-        <button onClick={() => markSuccess(false)}>Mission failed!</button>
+        <button onClick={() => addSuccess(true)}>Completed!</button>
+        <br />
+        <button onClick={() => addSuccess(false)}>Mission failed!</button>
         <br />
       </main>
       <br />
